9117424236f0c19bd942a1e257da691a7e18269e
tomomin1118 <tomomin1118@outlook.jp>
2021-10-03 08:15:02 +0000

    kadai 10

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfileContriller extends Controller
+class ProfileController extends Controller
 {
     //
     public function add()
